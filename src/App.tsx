fbddf889c40e39d9fdf37f24d3faaa2666d899e2
Iman Olabode-Bello <imanolabodebello8@gmail.com>
2026-05-09 21:53:27 +0100

    refactor: restructure App component to use AnimatedRoutes and integrate useLenis hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,44 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import CustomCursor from "./components/CustomCursor";
-import ThreeBackground from "./components/ThreeBackground";
+import { useLenis } from "./hooks/useLenis";
 
 const queryClient = new QueryClient();
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function AppInner() {
+  useLenis();
+  return (
+    <>
+      <CustomCursor src="/cursor.png" size={26} />
+      <Toaster />
+      <Sonner />
+      <AnimatedRoutes />
+    </>
+  );
+}
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <CustomCursor src="/cursor.png" size={26} />
-      {/* <ThreeBackground /> */}
-      <Toaster />
-      <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppInner />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
